867d2b6322d0c08c9b926748fd0ed9ed6d51c450
Christoph Thiele <thiele@math.uni-bonn.de>
2023-10-30 16:01:25 +0100

    small change

--- Carleson/HomogenousType.lean
+++ Carleson/HomogenousType.lean
@@ -8,13 +8,15 @@ local macro_rules | `($x ^ $y) => `(HPow.hPow $x $y) -- Porting note: See issue 
 /-- A space of homogenous type. -/
 class IsSpaceOfHomogenousType (X : Type*) (A : outParam ℝ≥0) [fact : Fact (1 ≤ A)] extends
   QuasiMetricSpace X A, MeasureSpace X, LocallyCompactSpace X, CompleteSpace X,
-  SigmaFinite (volume : Measure X), BorelSpace X where
+  /-SigmaFinite (volume : Measure X),-/ BorelSpace X where
   volume_ball_le : ∀ (x : X) r, volume (ball x (2 * r)) ≤ A * volume (ball x r)
   volume_ball_lt : ∀ (x : X) r, volume (ball x r) < ∞
 
 export IsSpaceOfHomogenousType (volume_ball_le)
 
 variable {X : Type*} {A : ℝ≥0} [fact : Fact (1 ≤ A)] [IsSpaceOfHomogenousType X A]
+
+instance : SigmaFinite (volume : Measure X) := sorry
 
 lemma test (x : X) (r : ℝ) : volume (ball x (4 * r)) ≤ A ^ 2 * volume (ball x r) := by
   calc volume (ball x (4 * r))
