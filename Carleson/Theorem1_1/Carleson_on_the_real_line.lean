1bf2e0a92f14dcc22623334e40dc0762ceb98661
Leo Diedering <leo.diedering@web.de>
2024-05-08 17:55:04 +0200

    Correct statement and proof of lemma Hilbert kernel bound.

--- Carleson/Theorem1_1/Carleson_on_the_real_line.lean
+++ Carleson/Theorem1_1/Carleson_on_the_real_line.lean
@@ -6,10 +6,12 @@ import Carleson.HomogeneousType
 --import Mathlib.Tactic
 import Mathlib.Analysis.Fourier.AddCircle
 import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.MeasureTheory.Integral.IntervalIntegral
+import Mathlib.Analysis.SpecialFunctions.Integrals
 
 
 noncomputable section
-local macro_rules | `($x ^ $y) => `(HPow.hPow $x $y) -- Porting note: See issue lean4#2220
+--local macro_rules | `($x ^ $y) => `(HPow.hPow $x $y) -- Porting note: See issue lean4#2220
 
 section
 
@@ -46,13 +48,15 @@ local notation "T_" => (CarlesonOperator K Θ)
 
 -- copied from HomogeneousType
 -- anyway, providing a proof would be good and is actually partly part of section 10.7
-instance helper {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E] :
-    IsSpaceOfHomogeneousType E (2 ^ FiniteDimensional.finrank ℝ E) := by sorry
+--instance helper {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [FiniteDimensional ℝ E] :
+--    IsSpaceOfHomogeneousType E (2 ^ FiniteDimensional.finrank ℝ E) := by sorry
 
 /- lemmas 10.22 to 10.26 are for this -/
+
 instance IsSpaceOfHomogeneousTypeR2: IsSpaceOfHomogeneousType ℝ 2 := by
-  have : IsSpaceOfHomogeneousType ℝ (2 ^ FiniteDimensional.finrank ℝ ℝ) := by apply helper
-  simpa
+  have : IsSpaceOfHomogeneousType ℝ (2 ^ FiniteDimensional.finrank ℝ ℝ) := by infer_instance
+  --simpa
+  exact {volume_ball_two_le_same := sorry}
 
 lemma h1 : 2 ∈ Set.Ioc 1 (2 : ℝ) := by simp
 lemma h2 : Real.IsConjExponent 2 2 := by rw [Real.isConjExponent_iff_eq_conjExponent] <;> norm_num
@@ -140,16 +144,18 @@ lemma localOscillation_of_integer_linear {x R: ℝ}  : ∀ n m : ℤ, localOscil
 
 
 /- to HomogeneousType -/
-lemma isSpaceOfHomogeneousType_with_increased_constant {X : Type} {a b : ℝ} [MetricSpace X] [IsSpaceOfHomogeneousType X a] (aleb : a ≤ b) : IsSpaceOfHomogeneousType X b where
+@[reducible]
+def isSpaceOfHomogeneousType_with_increased_constant {X : Type} {a b : ℝ} [MetricSpace X] [IsSpaceOfHomogeneousType X a] (aleb : a ≤ b) : IsSpaceOfHomogeneousType X b where
   volume_ball_two_le_same := by
     intro x r
     calc MeasureTheory.volume.real (Metric.ball x (2 * r))
     _ ≤ a * MeasureTheory.volume.real (Metric.ball x r) := by apply volume_ball_two_le_same
     _ ≤ b * MeasureTheory.volume.real (Metric.ball x r) := by gcongr
 
+--#lint
 --TODO : decide whether to move this up so that all the above lemmas are directly proven with the right doubling constant
 instance badR: IsSpaceOfHomogeneousType ℝ 4 := by
-  have : IsSpaceOfHomogeneousType ℝ (2 ^ FiniteDimensional.finrank ℝ ℝ) := by apply helper
+  have : IsSpaceOfHomogeneousType ℝ 2 := by infer_instance
   simp at this
   exact isSpaceOfHomogeneousType_with_increased_constant (by linarith)
 
@@ -187,15 +193,18 @@ instance h4 : IsCompatible Θ where
     let (a : Finset ℕ) := {1, 2, 3}
     /- The following is necessary for withLocalOscillation to be defined. -/
     have ball_bounded := fact_isCompact_ball x R
-    sorry
+    classical
     --TODO: fix problems with Finset
-    /-
-    let balls : Finset (C(ℝ, ℂ)) := {θ m₁, θ m₂, θ m₃} --with balls_def
+
+    set balls : Finset (C(ℝ, ℂ)) := {θ m₁, θ m₂, θ m₃} with balls_def
     use balls
     constructor
     . rw [balls_def]
-      apply card_le_three
-    -/
+      --simp
+      sorry
+      --apply Finset.card_le_three
+    . sorry
+
 
 
 --TODO : What is Lemma 10.34 (frequency ball growth) needed for?
@@ -212,19 +221,82 @@ lemma lower_secant_bound {η : ℝ} (ηpos : η > 0) {x : ℝ} (xIcc : x ∈ Set
 
 --open ComplexConjugate
 
+/- Lemma 10.37 (Hilbert kernel bound) -/
+lemma Hilbert_kernel_bound {x y : ℝ} : Complex.abs (K x y) ≤ 2 ^ (4 : ℝ) / (2 * |x - y|) := by
+  by_cases h : 0 < |x - y| ∧ |x - y| < 1
+  . calc Complex.abs (K x y)
+      _ ≤ 1 / Complex.abs (1 - Complex.exp (Complex.I * ↑(x - y))) := by
+        rw [K, k, Complex.abs.isAbsoluteValue.abv_div] --TODO: check whether the last lemma also has a better name
+        gcongr
+        rw [Complex.abs_ofReal, abs_of_nonneg]
+        . apply max_le
+          linarith [abs_nonneg (x-y)]
+          linarith
+        . apply le_max_right
+      _ ≤ 1 / (|x - y| / 8) := by
+        gcongr
+        . linarith
+        . apply lower_secant_bound
+          . exact h.1
+          . rw [Set.mem_Icc]
+            --TODO : improve calculations
+            constructor
+            . simp
+              calc |x - y|
+                _ ≤ 1 := h.2.le
+                _ ≤ 2 * Real.pi - 1 := by
+                  rw [le_sub_iff_add_le]
+                  linarith [Real.two_le_pi]
+                _ ≤ 2 * Real.pi + (x - y) := by
+                  rw [sub_eq_add_neg]
+                  gcongr
+                  exact (abs_le.mp h.2.le).1
+            . calc x - y
+                _ ≤ |x - y| := le_abs_self (x - y)
+                _ ≤ 1 := h.2.le
+                _ ≤ 2 * Real.pi - 1 := by
+                  rw [le_sub_iff_add_le]
+                  linarith [Real.two_le_pi]
+                _ ≤ 2 * Real.pi - |x - y| := by
+                  gcongr
+                  exact h.2.le
+          . trivial
+      _ = 8 / |x - y| := by
+        field_simp
+      _ = 2 ^ (4 : ℝ) / (2 * |x - y|) := by
+        rw [div_mul_eq_div_div]
+        congr
+        norm_num
+  . push_neg at h
+    have : Complex.abs (K x y) = 0 := by
+      rw [Complex.abs.isAbsoluteValue.abv_eq_zero, K, k, _root_.div_eq_zero_iff]
+      by_cases xeqy : x = y
+      . right
+        simp [xeqy]
+      . left
+        simp
+        apply h (abs_pos.mpr (sub_ne_zero.mpr xeqy))
+    rw [this]
+    apply div_nonneg
+    . norm_num
+    . linarith [abs_nonneg (x-y)]
+
 /- Lemma 10.38 (Hilbert kernel regularity) -/
-lemma Hilbert_kernel_regularity {x y y' : ℝ} :
+lemma Hilbert_kernel_regularity {x y y' : ℝ} (hxy : x ≠ y) :
     2 * |y - y'| ≤ |x - y| → ‖K x y - K x y'‖ ≤ 2 ^ 10 * (1 / |x - y|) * (|y - y'| / |x - y|)  := by
   rw [K, K]
   wlog x_eq_zero : x = 0 generalizing x y y'
   . intro h
-    set x_ := (0 : ℝ) with x_def
+    set! x_ := (0 : ℝ) with x_def
     set y_ := y - x with y_def
     set y'_ := y' - x with y'_def
+    have hxy_ : x_ ≠ y_ := by
+      rw [x_def, y_def]
+      exact (sub_ne_zero_of_ne hxy.symm).symm
     have h_ : 2 * |y_ - y'_| ≤ |x_ - y_| := by
       rw [x_def, y_def, y'_def]
       simpa
-    have := this x_def h_
+    have := this hxy_ x_def h_
     rw [x_def, y_def, y'_def] at this
     simpa
   rw [x_eq_zero]
@@ -251,7 +323,11 @@ lemma Hilbert_kernel_regularity {x y y' : ℝ} :
           rw [y_def, y'_def, ← abs_neg]
           simpa [neg_add_eq_sub]
         have y_y'_nonneg : 0 ≤ y_ ∧ 0 ≤ y'_ := by constructor <;> linarith
-        have := this h_ y_y'_nonneg
+        have hxy_ : x ≠ y_ := by
+          rw [x_eq_zero] at hxy
+          sorry
+
+        have := this hxy_ h_ y_y'_nonneg
         rw [y_def, y'_def] at this
         simp only [neg_neg, abs_neg, sub_neg_eq_add, neg_add_eq_sub] at this
         rw [← IsROrC.norm_conj, map_sub, ← k_of_neg_eq_conj_k, ← k_of_neg_eq_conj_k, ←abs_neg (y' - y)] at this
@@ -274,7 +350,17 @@ lemma Hilbert_kernel_regularity {x y y' : ℝ} :
   . rw [k_of_abs_le_one, k_of_abs_le_one]
     . simp only [abs_neg, Complex.ofReal_neg, mul_neg, ge_iff_le]
       rw [abs_of_nonneg yy'nonneg.1, abs_of_nonneg yy'nonneg.2]
-      sorry
+      let κ : ℝ → ℂ := fun t ↦ (1 - t) / (1 - Complex.exp (-(Complex.I * t)))
+      let κ' : ℝ → ℂ := fun t ↦ (-1 + Complex.exp (-Complex.I * t) + Complex.I * (1 - t) * Complex.exp (Complex.I * t)) / (1 - Complex.exp (-(Complex.I * t)))^2
+      calc ‖(1 - y) / (1 - Complex.exp (-(Complex.I * y))) - (1 - y') / (1 - Complex.exp (-(Complex.I * y')))‖
+      _ = ‖κ y - κ y'‖ := by simp
+      _ = ‖∫ (t : ℝ) in y'..y, κ' t‖ := by
+        congr 1
+        rw [←intervalIntegral.integral_eq_sub_of_hasDerivAt]
+        . intro t ht
+          sorry
+        . sorry
+      _ ≤ 2 ^ 10 * (1 / y) * (|y - y'| / y) := by sorry
     . rw [abs_neg, abs_of_nonneg yy'nonneg.2]
       assumption
     . rw [abs_neg, abs_of_nonneg yy'nonneg.1]
@@ -284,6 +370,7 @@ lemma Hilbert_kernel_regularity {x y y' : ℝ} :
   . sorry
 
 
+#check intervalIntegral.integral_eq_sub_of_hasDerivAt
 
 --TODO : add some Real.vol lemma
 
@@ -291,70 +378,10 @@ instance h6 : IsCZKernel 4 K where
   /- Lemma 10.37 (Hilbert kernel bound) from the paper. -/
   norm_le_vol_inv := by
     intro x y
-    by_cases h : 0 < |x - y| ∧ |x - y| < 1
-    . calc ‖K x y‖
-        _ ≤ 1 / ‖1 - Complex.exp (Complex.I * ↑(x - y))‖ := by
-          rw [K, k, norm_div]
-          gcongr
-          rw [Complex.norm_real, Real.norm_eq_abs, abs_of_nonneg]
-          . apply max_le
-            linarith [abs_nonneg (x-y)]
-            linarith
-          . apply le_max_right
-        _ ≤ 1 / (|x - y| / 8) := by
-          gcongr
-          . linarith
-          . apply lower_secant_bound
-            . exact h.1
-            . rw [Set.mem_Icc]
-              --TODO : improve calculations
-              constructor
-              . simp
-                calc |x - y|
-                  _ ≤ 1 := h.2.le
-                  _ ≤ 2 * Real.pi - 1 := by
-                    rw [le_sub_iff_add_le]
-                    linarith [Real.two_le_pi]
-                  _ ≤ 2 * Real.pi + (x - y) := by
-                    rw [sub_eq_add_neg]
-                    gcongr
-                    exact (abs_le.mp h.2.le).1
-              . calc x - y
-                  _ ≤ |x - y| := le_abs_self (x - y)
-                  _ ≤ 1 := h.2.le
-                  _ ≤ 2 * Real.pi - 1 := by
-                    rw [le_sub_iff_add_le]
-                    linarith [Real.two_le_pi]
-                  _ ≤ 2 * Real.pi - |x - y| := by
-                    gcongr
-                    exact h.2.le
-            . trivial
-        _ = 8 / |x - y| := by
-          field_simp
-        _ ≤ (2 : ℝ) ^ (4 : ℝ) ^ 3 / Real.vol x y := by
-          rw [Real.vol, MeasureTheory.measureReal_def, Real.dist_eq, Real.volume_ball, ENNReal.toReal_ofReal]
-          . ring_nf
-            gcongr
-            norm_num
-          . linarith
-        _ = (2 : ℝ) ^ (4 : ℝ) ^ 3 / @Real.vol ℝ Real.pseudoMetricSpace IsSpaceOfHomogeneousType.toMeasureSpace x y := by
-          congr
-          --TODO : Identify Real.measureSpace = IsSpaceOfHomogeneousType.toMeasureSpace
-          --rw [IsSpaceOfHomogeneousType.toMeasureSpace, badR]
-          sorry
-    . push_neg at h
-      have : ‖K x y‖ = 0 := by
-        rw [norm_eq_zero, K, k, _root_.div_eq_zero_iff]
-        by_cases xeqy : x = y
-        . right
-          simp [xeqy]
-        . left
-          simp
-          apply h (abs_pos.mpr (sub_ne_zero.mpr xeqy))
-      rw [this]
-      apply div_nonneg
-      . norm_num
-      . exact MeasureTheory.measureReal_nonneg
+    rw [Complex.norm_eq_abs, Real.vol, MeasureTheory.measureReal_def, Real.dist_eq, Real.volume_ball, ENNReal.toReal_ofReal (by linarith [abs_nonneg (x-y)])]
+    calc Complex.abs (K x y)
+    _ ≤ 2 ^ (4 : ℝ) / (2 * |x - y|) := by apply Hilbert_kernel_bound
+    _ ≤ 2 ^ (4 : ℝ) ^ 3 / (2 * |x - y|) := by gcongr <;> norm_num
   /- uses Lemma 10.38 (Hilbert kernel regularity) -/
   norm_sub_le := by sorry
   /- Lemma ?-/
