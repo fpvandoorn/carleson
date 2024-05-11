9defdeddac6a3df4144b73cb3484d4bcde9dd711
Leo Diedering <leo.diedering@web.de>
2024-05-11 16:20:59 +0200

    Split the main file, part 1.

---
+++ Carleson/Theorem1_1/Basic.lean
@@ -0,0 +1,38 @@
+import Carleson.Carleson
+import Carleson.HomogeneousType
+
+--import Mathlib.Tactic
+import Mathlib.Analysis.Fourier.AddCircle
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Analysis.Convolution
+import Mathlib.Analysis.Calculus.BumpFunction.Convolution
+import Mathlib.Analysis.Calculus.BumpFunction.FiniteDimension
+import Mathlib.Analysis.PSeries
+
+open BigOperators
+open Finset
+
+noncomputable section
+
+def partialFourierSum (f : ℝ → ℂ) (N : ℕ) : ℝ → ℂ := fun x ↦ ∑ n in Icc (-Int.ofNat ↑N) N, fourierCoeffOn Real.two_pi_pos f n * fourier n (x : AddCircle (2 * Real.pi))
+--fun x ↦ ∑ n in Icc (-Int.ofNat ↑N) N, fourierCoeffOn Real.two_pi_pos f n * fourier n (x : AddCircle (2 * Real.pi))
+#check partialFourierSum
+
+
+lemma fourier_periodic {n : ℤ} : Function.Periodic (fun (x : ℝ) ↦ fourier n (x : AddCircle (2 * Real.pi))) (2 * Real.pi) := by
+  intro x
+  simp
+
+lemma partialFourierSum_periodic {f : ℝ → ℂ} {N : ℕ} : Function.Periodic (partialFourierSum f N) (2 * Real.pi) := by
+  rw [Function.Periodic]
+  intro x
+  rw [partialFourierSum, partialFourierSum]
+  congr
+  ext n
+  congr 1
+  exact fourier_periodic x
+
+variable {f : ℝ → ℂ} {N : ℕ}
+
+--TODO : add reasonable notation
+--local notation "S_" => partialFourierSum f
