eada2992c0b94c040601e8036b202c4c46af9eaa
mariainesdff <mariaines.dff@gmail.com>
2024-06-20 15:12:14 +0200

    work on lemma statements

--- Carleson/AntichainOperator.lean
+++ Carleson/AntichainOperator.lean
@@ -24,3 +24,57 @@ lemma E_disjoint (σ σ' : X → ℤ) {𝔄 : Set (𝔓 X)} (h𝔄 : IsAntichain
     (or_iff_right (not_disjoint_iff.mpr ⟨Q x, hxΩp, hxΩp'⟩)).mp (relative_fundamental_dyadic h𝓓)
   have hle : p ≤ p' := ⟨h𝓓, hΩ⟩
   exact IsAntichain.eq h𝔄 hp hp' hle
+
+variable (K : X → X → ℂ) (σ₁ σ₂ : X → ℤ) (ψ : ℝ → ℝ) (p : 𝔓 X)
+--(f : X → ℂ) (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x)
+
+-- lemma 6.1.2
+-- Q : `p : 𝔄` or `p ∈ 𝔄`?
+lemma MaximalBoundAntichain {a : ℝ} (ha : 4 ≤ a) {𝔄 : Set (𝔓 X)} (h𝔄 : IsAntichain (·≤·) 𝔄)
+    {F : Set X} {f : X → ℂ} (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x) (x : X) :
+    Complex.abs (∑' (p : 𝔄), T K σ₁ σ₂ ψ p F f x) ≤ 2^(107*a^3)/-*M_B (f x)-/ := by
+  by_cases hx : ∃ (p : 𝔄), T K σ₁ σ₂ ψ p F f x ≠ 0
+  · obtain ⟨p, hpx⟩ := hx
+    have hne_p : ∀ (p' : 𝔄) (hp' : p' ≠ p), T K σ₁ σ₂ ψ (↑p') F f x = 0 := by
+      intro p' hpp'
+      sorry
+    sorry
+  · simp only [ne_eq, Subtype.exists, exists_prop, not_exists, not_and, Decidable.not_not] at hx
+    have h0 : (∑' (p : 𝔄), T K σ₁ σ₂ ψ p F f x) = (∑' (p : 𝔄), 0)  := by
+      congr
+      ext p
+      exact hx p p.2
+    rw [h0]
+    sorry--simp only [tsum_zero, map_zero, ge_iff_le, Nat.ofNat_nonneg, pow_nonneg]
+
+lemma _root_.Set.eq_indicator_one_mul {F : Set X} {f : X → ℂ} (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x) :
+    f = (F.indicator 1) * f := by
+  ext y
+  simp only [Pi.mul_apply, indicator, Pi.one_apply, ite_mul, one_mul, zero_mul]
+  split_ifs with hy
+  · rfl
+  · specialize hf y
+    simp only [indicator, hy, ↓reduceIte] at hf
+    rw [← norm_eq_zero]
+    exact le_antisymm hf (norm_nonneg _)
+
+--  MeasureTheory.snorm
+-- lemma 6.1.3
+lemma Dens2Antichain  {a : ℝ} (ha : 4 ≤ a) {q : ℝ} (hq1 : 1 < q) (hq2 : q ≤ 2) {𝔄 : Set (𝔓 X)}
+    (h𝔄 : IsAntichain (·≤·) 𝔄) {F : Set X} {f : X → ℂ} (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x)
+    {G : Set X} {g : X → ℂ} (hg : ∀ x, ‖g x‖ ≤ G.indicator 1 x) (x : X) :
+    Complex.abs (∫ x, ((starRingEnd ℂ) (g x)) * ∑' (p : 𝔄), T K σ₁ σ₂ ψ p F f x) ≤
+      2^(111*a^3)*(q-1)⁻¹/-* dens2(𝔄)^{1/q - 1/2} *‖f‖_2*‖g‖_2}-/  := by
+  have hf1 : f = (F.indicator 1) * f := eq_indicator_one_mul hf
+  set q' := 2*q/(1 + q) with hq'
+  have hq0 : 0 < q := lt_trans zero_lt_one hq1
+  have h1q' : 1 ≤ q' := by -- Better proof?
+    rw [hq', one_le_div]
+    linarith
+    exact add_pos (zero_lt_one) hq0
+  have hqq' : q' ≤ q := by -- Better proof?
+    rw [hq', div_le_iff (add_pos (zero_lt_one) hq0), mul_comm, mul_le_mul_iff_of_pos_left hq0]
+    linarith
+  sorry
+
+-- ‖∫ x in G \ G', ∑' p, T K σ₁ σ₂ (ψ (D2_2 a)) p F 1 x‖₊ ≤
