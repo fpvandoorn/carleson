40084f584b94f6f996ae822dec99c47fa5cd4d45
Parcly Taxel <reddeloostw@gmail.com>
2024-07-02 19:44:50 +0800

    flesh out more wiggle order lemmas

--- Carleson/DiscreteCarleson.lean
+++ Carleson/DiscreteCarleson.lean
@@ -12,15 +12,20 @@ open scoped ShortVariables
 variable {X : Type*} {a : ℕ} {q : ℝ} {K : X → X → ℂ} {σ₁ σ₂ : X → ℤ} {F G : Set X}
   [MetricSpace X] [ProofData a q K σ₁ σ₂ F G] [TileStructure Q D κ S o]
 
+section WiggleOrder
+
+variable {p p' : 𝔓 X} {m m' n n' : ℝ}
+
 /-- Lemma 5.3.1 -/
-lemma smul_mono {p p' : 𝔓 X} {m m' n n' : ℝ} (hp : smul n p ≤ smul m p')
-    (hm : m' ≤ m) (hn : n ≤ n') : smul n' p ≤ smul m' p' := by
+lemma smul_mono (hp : smul n p ≤ smul m p') (hm : m' ≤ m) (hn : n ≤ n') :
+    smul n' p ≤ smul m' p' := by
   change 𝓘 p ≤ 𝓘 p' ∧ ball_(p') (𝒬 p') m ⊆ ball_(p) (𝒬 p) n at hp
   change 𝓘 p ≤ 𝓘 p' ∧ ball_(p') (𝒬 p') m' ⊆ ball_(p) (𝒬 p) n'
   exact ⟨hp.1, (ball_subset_ball hm).trans (hp.2.trans (ball_subset_ball hn))⟩
 
+variable (m) in
 /-- Lemma 5.3.2 -/
-lemma smul_C2_1_2 {p p' : 𝔓 X} {m n : ℝ} (hp : 𝓘 p ≠ 𝓘 p') (hl : smul n p ≤ smul 1 p') :
+lemma smul_C2_1_2 (hp : 𝓘 p ≠ 𝓘 p') (hl : smul n p ≤ smul 1 p') :
     smul (n + C2_1_2 a * m) p ≤ smul m p' := by
   replace hp : 𝓘 p < 𝓘 p' := lt_of_le_of_ne hl.1 hp
   have : ball_(p') (𝒬 p') m ⊆ ball_(p) (𝒬 p) (n + C2_1_2 a * m) := fun x hx ↦ by
@@ -34,6 +39,38 @@ lemma smul_C2_1_2 {p p' : 𝔓 X} {m n : ℝ} (hp : 𝓘 p ≠ 𝓘 p') (hl : sm
         rw [add_comm]; gcongr
         exact mem_ball.mp <| mem_of_mem_of_subset (by convert mem_ball_self zero_lt_one) hl.2
   exact ⟨hl.1, this⟩
+
+/-- Lemma 5.3.3, Equation (5.3.3) -/
+lemma wiggle_order_11_10 (hp : p ≤ p') (hn : 11 / 10 ≤ n) : smul n p ≤ smul n p' := by
+  sorry
+
+/-- Lemma 5.3.3, Equation (5.3.4) -/
+lemma wiggle_order_100 (hp : smul 10 p ≤ smul 1 p') (hn : 𝓘 p ≠ 𝓘 p') :
+    smul 100 p ≤ smul 100 p' := by
+  calc
+    _ ≤ smul (10 + C2_1_2 a * 100) p := by
+      refine smul_mono le_rfl le_rfl ?_
+      have : C2_1_2 a ≤ 1 / 2 := by
+        rw [C2_1_2, show (1 / 2 : ℝ) = 2 ^ (-1 : ℝ) by norm_num,
+          Real.rpow_le_rpow_left_iff one_lt_two, neg_mul, neg_le_neg_iff]
+        norm_cast; linarith [four_le_a X]
+      linarith
+    _ ≤ _ := smul_C2_1_2 100 hn hp
+
+/-- Lemma 5.3.3, Equation (5.3.5) -/
+lemma wiggle_order_500 (hp : smul 2 p ≤ smul 1 p') (hn : 𝓘 p ≠ 𝓘 p') :
+    smul 4 p ≤ smul 500 p' := by
+  calc
+    _ ≤ smul (2 + C2_1_2 a * 500) p := by
+      refine smul_mono le_rfl le_rfl ?_
+      have : C2_1_2 a ≤ 1 / 512 := by
+        rw [C2_1_2, show (1 / 512 : ℝ) = 2 ^ (-9 : ℝ) by norm_num,
+          Real.rpow_le_rpow_left_iff one_lt_two, neg_mul, neg_le_neg_iff]
+        norm_cast; linarith [four_le_a X]
+      linarith
+    _ ≤ _ := smul_C2_1_2 500 hn hp
+
+end WiggleOrder
 
 /- The constant used in Proposition 2.0.2 -/
 def C2_0_2 (a : ℝ) (q : ℝ) : ℝ := 2 ^ (440 * a ^ 3) / (q - 1) ^ 5
