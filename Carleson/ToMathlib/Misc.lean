d90a0fe3f457d4c39a146af1296e7b0c7dbe5734
js2357 <James.Sundstrom@gmail.com>
2024-07-07 17:13:49 -0400

    Proof of Lemma 2.1.3 and other results in Psi.lean

--- Carleson/ToMathlib/Misc.lean
+++ Carleson/ToMathlib/Misc.lean
@@ -26,6 +26,17 @@ theorem Int.floor_le_iff (c : ℝ) (z : ℤ) : ⌊c⌋ ≤ z ↔ c < z + 1 := by
 theorem Int.le_ceil_iff (c : ℝ) (z : ℤ) : z ≤ ⌈c⌉ ↔ z - 1 < c := by
   rw_mod_cast [← Int.add_one_le_ceil_iff, sub_add_cancel]
 
+theorem Int.Icc_of_eq_sub_1 {a b : ℤ} (h : a = b - 1) : Finset.Icc a b = {a, b} := by
+  refine le_antisymm (fun t ht ↦ ?_) (fun t ht ↦ ?_)
+  · rw [h, Finset.mem_Icc] at ht
+    by_cases hta : t = b - 1
+    · rw [hta, ← h]; exact Finset.mem_insert_self a {b}
+    · suffices b = t from this ▸ Finset.mem_insert.2 (Or.inr (Finset.mem_singleton.2 rfl))
+      exact le_antisymm (sub_add_cancel b 1 ▸ (ne_eq t (b - 1) ▸ hta).symm.lt_of_le ht.1) ht.2
+  · have hab : a ≤ b := h ▸ sub_le_self b one_pos.le
+    rcases Finset.mem_insert.1 ht with rfl | hb
+    · exact Finset.mem_Icc.2 ⟨le_refl t, hab⟩
+    · rw [Finset.mem_singleton.1 hb]; exact Finset.mem_Icc.2 ⟨hab, le_refl b⟩
 
 lemma tsum_one_eq' {α : Type*} (s : Set α) : ∑' (_:s), (1 : ℝ≥0∞) = s.encard := by
   if hfin : s.Finite then
@@ -67,3 +78,7 @@ lemma ENNReal.tsum_const_eq' {α : Type*} (s : Set α) (c : ℝ≥0∞) :
     ∑' (_:s), (c : ℝ≥0∞) = s.encard * c := by
   nth_rw 1 [← one_mul c]
   rw [ENNReal.tsum_mul_right,tsum_one_eq']
+
+theorem PseudoMetricSpace.dist_eq_of_dist_zero {X : Type*} [PseudoMetricSpace X] (x : X) {y y' : X}
+    (hyy' : dist y y' = 0) : dist x y = dist x y' :=
+  dist_comm y x ▸ dist_comm y' x ▸ sub_eq_zero.1 (abs_nonpos_iff.1 (hyy' ▸ abs_dist_sub_le y y' x))
