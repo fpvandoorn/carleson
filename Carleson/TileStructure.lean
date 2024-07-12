bfe705002a5390fe69dd5129b6d21c733ba50ca1
js2357 <James.Sundstrom@gmail.com>
2024-07-12 04:48:54 -0400

    Prove tile_sum_operator

--- Carleson/TileStructure.lean
+++ Carleson/TileStructure.lean
@@ -86,7 +86,12 @@ It is the map `T ∘ (1_F * ·) : f ↦ T (1_F * f)`, also denoted `T1_F`
 The operator `T` in Proposition 2.0.2 is therefore `applied to `(F := Set.univ)`. -/
 def T (p : 𝔓 X) (f : X → ℂ) : X → ℂ :=
   indicator (E p)
-    fun x ↦ ∫ y, exp (Q x x - Q x y) * K x y * ψ (D ^ (- 𝔰 p) * dist x y) * F.indicator f y
+    fun x ↦ ∫ y, exp (I * (Q x y - Q x x)) * K x y * ψ (D ^ (- 𝔰 p) * dist x y) * F.indicator f y
+
+lemma T_def' (p : 𝔓 X) (f : X → ℂ) : T p f =
+    indicator (E p) fun x ↦ ∫ y, Ks (𝔰 p) x y * F.indicator f y * exp (I * (Q x y - Q x x)) := by
+  unfold T Ks
+  exact congr_arg _ (funext fun x ↦ (congr_arg _ (funext fun y ↦ by ring)))
 
 end T
 
