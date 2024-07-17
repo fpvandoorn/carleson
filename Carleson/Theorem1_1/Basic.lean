2e6e20de75deb3df52a584371f773f5a873a670e
Leo Diedering <leo.diedering@web.de>
2024-07-17 22:07:55 +0200

    Added file for spectral projection bound. Should be mostly in mathlib.

--- Carleson/Theorem1_1/Basic.lean
+++ Carleson/Theorem1_1/Basic.lean
@@ -12,6 +12,11 @@ noncomputable section
 --TODO : add reasonable notation
 --local notation "S_" => partialFourierSum f
 
+--TODO: use this as base to build on?
+def AddCircle.partialFourierSum' {T : ℝ} [hT : Fact (0 < T)] (f : AddCircle T → ℂ) (N : ℕ) (x : AddCircle T) : ℂ :=
+    ∑ n in Icc (-Int.ofNat ↑N) N, fourierCoeff f n * fourier n (x)
+
+--TODO: switch N and f
 def partialFourierSum (f : ℝ → ℂ) (N : ℕ) : ℝ → ℂ := fun x ↦ ∑ n in Icc (-Int.ofNat ↑N) N, fourierCoeffOn Real.two_pi_pos f n * fourier n (x : AddCircle (2 * Real.pi))
 --fun x ↦ ∑ n in Icc (-Int.ofNat ↑N) N, fourierCoeffOn Real.two_pi_pos f n * fourier n (x : AddCircle (2 * Real.pi))
 
---
+++ Carleson/Theorem1_1/SpectralProjectionBound.lean
@@ -0,0 +1,16 @@
+import Carleson.Theorem1_1.Basic
+
+open MeasureTheory AddCircle
+
+--TODO: add version of partialFourierSum for the AddCircle?
+--TODO: completely reformulate partialFourierSum in terms of more abstract structures?
+
+lemma spectral_projection_bound {N : ℕ} {T : ℝ} [hT : Fact (0 < T)] (f : Lp ℂ 2 <| @haarAddCircle T hT) :
+    ∫ t : AddCircle T, ‖partialFourierSum' f N t‖ ^ 2 ∂haarAddCircle ≤ ∫ t : AddCircle T, ‖f t‖ ^ 2 ∂haarAddCircle := by
+  --calc ‖partialFourierSum' f N t‖
+  sorry
+
+
+#check hasSum_fourier_series_L2
+#check tsum_sq_fourierCoeff
+#check hasSum_fourier_series_of_summable
