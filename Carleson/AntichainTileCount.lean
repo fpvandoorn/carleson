ff4220c1dd84241794b5f346ace508885e782d89
mariainesdff <mariaines.dff@gmail.com>
2024-10-15 13:35:38 +0200

    state 6.3.1, 6.3.4

---
+++ Carleson/AntichainTileCount.lean
@@ -0,0 +1,43 @@
+import Carleson.TileStructure
+import Carleson.HardyLittlewood
+import Carleson.Psi
+
+noncomputable section
+
+open scoped ENNReal ShortVariables
+
+open GridStructure MeasureTheory Set
+
+section
+
+variable {X : Type*} {a : ℕ} {q : ℝ} {K : X → X → ℂ} {σ₁ σ₂ : X → ℤ} {F G : Set X}
+  [MetricSpace X] [ProofData a q K σ₁ σ₂ F G] [TileStructure Q D κ S o]
+
+-- Lemma 6.3.1
+lemma tile_reach {ϑ : Θ X} {N : ℕ} {p p' : 𝔓 X} (hp : dist_(p) (𝒬 p) ϑ ≤ 2^N)
+    (hp' : dist_(p') (𝒬 p') ϑ ≤ 2^N) (hI : 𝓘 p ≤ 𝓘 p') (hs : 𝔰 p < 𝔰 p'):
+  smul (2^(N + 2)) p ≤ smul (2^(N + 2)) p' := sorry
+
+-- Q : should this be a subset of 𝔄 instead? Let's see which version makes things easier
+-- Def 6.3.15
+def 𝔄_aux (𝔄 : Finset (𝔓 X)) (ϑ : Θ X) (N : ℕ) : Set (𝔓 X) :=
+  {p : 𝔓 X | p ∈ 𝔄 ∧ 1 + dist_(p) (𝒬 p) ϑ ∈ Icc (2^N) (2^(N+1))}
+
+-- TODO: ask about sum
+open Classical
+-- Lemma 6.3.2
+lemma stack_density (𝔄 : Finset (𝔓 X)) (ϑ : Θ X) (N : ℕ) (L : Grid X) :
+    ∑ (p ∈ {p : 𝔓 X | p ∈ (𝔄_aux 𝔄 ϑ N) ∧ 𝓘 p = L}), volume (E p ∩ G) ≤
+      2^(a * (N + 5)) * dens₁ (𝔄 : Set (𝔓 X)) * volume (coeGrid L) := sorry
+
+-- Lemma 6.3.3
+lemma local_antichain_density (𝔄 : Finset (𝔓 X)) (ϑ : Θ X) (N : ℕ) : False := sorry
+
+def C_6_3_4 (a N : ℕ) : ℝ≥0∞ := 2^(101*a^3 + N*a)
+
+-- Lemma 6.3.4
+lemma global_antichain_density (𝔄 : Finset (𝔓 X)) (ϑ : Θ X) (N : ℕ) :
+    ∑ (p ∈ 𝔄_aux 𝔄 ϑ N), volume (E p ∩ G) ≤
+      (C_6_3_4 a N) * dens₁ (𝔄 : Set (𝔓 X)) * volume (⋃ (p ∈ 𝔄), coeGrid (𝓘 p)) := sorry
+
+-- Lemma 6.1.6
