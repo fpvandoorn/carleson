698875e5f1103c54966d647658ce4ba44a808ca4
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-10-16 22:54:42 +0200

    Remove unnecessary step in update_mathlib scripts

--- scripts/update_mathlib.sh
+++ scripts/update_mathlib.sh
@@ -3,4 +3,3 @@
 # Update mathlib and the lean toolchain.
 curl -L https://raw.githubusercontent.com/leanprover-community/mathlib4/master/lean-toolchain -o lean-toolchain
 lake -R -Kenv=dev update # The `-R -Kenv=dev` is making sure we also update doc-gen and other dependencies
-lake exe cache get
