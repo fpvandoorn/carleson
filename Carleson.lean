aad98a6a4197f390595c9dee1c81eb1e34059efa
mariainesdff <mariaines.dff@gmail.com>
2024-10-28 11:26:10 +0100

    fix imports

--- Carleson.lean
+++ Carleson.lean
@@ -1,4 +1,5 @@
-import Carleson.AntichainOperator
+import Carleson.Antichain.AntichainOperator
+import Carleson.Antichain.TileCorrelation
 import Carleson.Classical.Approximation
 import Carleson.Classical.Basic
 import Carleson.Classical.CarlesonOnTheRealLine
