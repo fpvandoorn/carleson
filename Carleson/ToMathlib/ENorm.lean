a0c6a93343f4d6b96e4015f744d28dde0d92dfe5
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2024-11-06 22:27:20 +0100

    Add lemmas `wnorm'_zero` and `wnorm_zero `

--- Carleson/ToMathlib/ENorm.lean
+++ Carleson/ToMathlib/ENorm.lean
@@ -93,6 +93,16 @@ def wnorm' [ENorm E] (f : α → E) (p : ℝ) (μ : Measure α) : ℝ≥0∞ :=
 def wnorm [ENorm E] (f : α → E) (p : ℝ≥0∞) (μ : Measure α) : ℝ≥0∞ :=
   if p = ∞ then eLpNormEssSup f μ else wnorm' f (ENNReal.toReal p) μ
 
+lemma wnorm'_zero [ENorm E] (f : α → E) (μ : Measure α) : wnorm' f 0 μ = ∞ := by
+  simp only [wnorm, zero_ne_top, ↓reduceIte, wnorm', zero_toReal, GroupWithZero.inv_zero,
+    ENNReal.rpow_zero, mul_one, iSup_eq_top]
+  refine fun b hb ↦ ⟨b.toNNReal + 1, ?_⟩
+  rw [coe_add, ENNReal.coe_one, coe_toNNReal hb.ne_top]
+  exact lt_add_right hb.ne_top one_ne_zero
+
+lemma wnorm_zero [ENorm E] (f : α → E) (μ : Measure α) : wnorm f 0 μ = ∞ := by
+  simp [wnorm, wnorm'_zero]
+
 /-- A function is in weak-L^p if it is (strongly a.e.)-measurable and has finite weak L^p norm. -/
 def MemWℒp [TopologicalSpace E] [ENorm E] (f : α → E) (p : ℝ≥0∞) (μ : Measure α) : Prop :=
   AEStronglyMeasurable f μ ∧ wnorm f p μ < ∞
@@ -130,48 +140,53 @@ not infinity (this should be relatively easy from unfolding the definitions).
 /- If a function into `ENNReal` is `MemWℒp`, then its norm almost everywhere not infinity.-/
 lemma MemWℒp.ae_ne_top [TopologicalSpace E] [ENorm E] {f : α → E} {p : ℝ≥0∞} {μ : Measure α}
     (hf : MemWℒp f p μ) : ∀ᵐ x ∂μ, ‖f x‖ₑ ≠ ∞ := by
-  by_cases hp : p = ∞
-  · rw [hp] at hf
+  by_cases hp_inf : p = ∞
+  · rw [hp_inf] at hf
     simp_rw [← lt_top_iff_ne_top]
     exact ae_lt_of_essSup_lt hf.2
-  · set A := { x | ‖f x‖ₑ = ∞ } with hA
-    unfold MemWℒp wnorm wnorm' at hf
-    simp [hp] at hf
-    rw [Filter.eventually_iff, mem_ae_iff]
-    simp [compl_def, ← hA]
-    by_contra h
-    have h1 (t : ℝ≥0) : μ A ≤ distribution f t μ := by
-      refine μ.mono ?_
-      simp_all only [setOf_subset_setOf, coe_lt_top, implies_true, A]
-    set C := ⨆ t : ℝ≥0, t * distribution f t μ ^ p.toReal⁻¹ with hC
-    have h2 : C < ∞ := by aesop
-    -- maybe separate the case C = 0?
-    have h3' (t : ℝ≥0) : t * distribution f t μ ^ p.toReal⁻¹ ≤ C := le_iSup_iff.mpr fun b a ↦ a t
-    have h3 (t : ℝ≥0) (ht : t ≠ 0) : distribution f t μ ≤ (C / t) ^ p.toReal := by
-      sorry
-    have h4 (t : ℝ≥0) (ht : t ≠ 0) : μ A ≤ (C / t) ^ p.toReal := (h1 t).trans (h3 t ht)
-    have h5 : μ A ≤ μ A / 2 := by
-      convert h4 (C * (2 / μ A) ^ p.toReal⁻¹).toNNReal ?_
-      swap
-      · sorry
-      refine ?_
-      rw [ENNReal.coe_toNNReal]
-      swap
-      · refine mul_ne_top h2.ne_top (rpow_ne_top_of_nonneg (inv_nonneg.mpr toReal_nonneg) ?_)
-        simp [div_eq_top, h]
-      refine ?_
-      nth_rw 1 [← mul_one C]
-      rw [ENNReal.mul_div_mul_left]
-      rotate_left
-      · sorry
-      · exact h2.ne_top
-      -- simp  [toNNReal_mul, toNNReal_rpow, toNNReal_div, coe_mul]
-      refine ?_
-      sorry -- use t = (C * (2 / μ A) ^ p.toReal⁻¹)
-    -- Find a way to make a contradiction from h5, it is mathematically clear, we need a lemma from
-    -- Mathlib that says that h5 → μ A = 0, then the contradiction comes from h
-    have h6 : μ A = 0 := by sorry
-    exact h h6
+  set A := { x | ‖f x‖ₑ = ∞ } with hA
+  unfold MemWℒp wnorm wnorm' at hf
+  simp [hp_inf] at hf
+  rw [Filter.eventually_iff, mem_ae_iff]
+  simp [compl_def, ← hA]
+  by_contra h
+  have h1 (t : ℝ≥0) : μ A ≤ distribution f t μ := by
+    refine μ.mono ?_
+    simp_all only [setOf_subset_setOf, coe_lt_top, implies_true, A]
+  set C := ⨆ t : ℝ≥0, t * distribution f t μ ^ p.toReal⁻¹ with hC
+  have h2 : C < ∞ := by aesop
+  -- maybe separate the case C = 0?
+  have h3' (t : ℝ≥0) : t * distribution f t μ ^ p.toReal⁻¹ ≤ C := le_iSup_iff.mpr fun b a ↦ a t
+  have h3 (t : ℝ≥0) (ht : t ≠ 0) : distribution f t μ ≤ (C / t) ^ p.toReal := by
+    sorry
+  have h4 (t : ℝ≥0) (ht : t ≠ 0) : μ A ≤ (C / t) ^ p.toReal := (h1 t).trans (h3 t ht)
+  have h5 : μ A ≤ μ A / 2 := by
+    convert h4 (C * (2 / μ A) ^ p.toReal⁻¹).toNNReal ?_
+    swap
+    · sorry
+    refine ?_
+    rw [ENNReal.coe_toNNReal]
+    swap
+    · refine mul_ne_top h2.ne_top (rpow_ne_top_of_nonneg (inv_nonneg.mpr toReal_nonneg) ?_)
+      simp [div_eq_top, h]
+    refine ?_
+    nth_rw 1 [← mul_one C]
+    rw [ENNReal.mul_div_mul_left]
+    rotate_left
+    · sorry
+    · exact h2.ne_top
+    -- simp  [toNNReal_mul, toNNReal_rpow, toNNReal_div, coe_mul]
+    simp? [div_rpow_of_nonneg, - one_div]
+
+    refine ?_
+    rw [ENNReal.rpow_inv_rpow]
+    refine ?_
+    rw [div_rpow_of_nonneg]
+    sorry -- use t = (C * (2 / μ A) ^ p.toReal⁻¹)
+  -- Find a way to make a contradiction from h5, it is mathematically clear, we need a lemma from
+  -- Mathlib that says that h5 → μ A = 0, then the contradiction comes from h
+  have h6 : μ A = 0 := by sorry
+  exact h h6
 
 /-
 2. Prove a variant `HasWeakType.MB_one` but for the function `MB` that
