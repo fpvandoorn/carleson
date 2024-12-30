9ec9c6e4d9ce629c55a6245928c3afb79f48c838
Evgenia Karunus <lakesare@gmail.com>
2024-12-30 17:12:19 +0500

    Lemma 7.5.1 (#194)
    [[blueprint]](https://florisvandoorn.com/carleson/blueprint/treesection.html#dyadic-partition-1)
    
    This can be merged!
    PR review is appreciated.
    
    - [x] prove the remaining `𝓘 u₁ ⊂ cube` subgoal (`lemma union_𝓙₅`)
    - [x] prove the second theorem (`lemma pairwiseDisjoint_𝓙₅`)
    - [x] add `\leanok`
    - [x] refactor (`lemma union_𝓙₅`)
    - [x] refactor (`lemma pairwiseDisjoint_𝓙₅`)
    - [x] update the blueprint to not use strict containment
    - [x] address PR review suggestions
    
    ---------
    
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>

--- Carleson/Forest.lean
+++ Carleson/Forest.lean
@@ -62,6 +62,7 @@ lemma lt_dist (hu : u ∈ t) (hu' : u' ∈ t) (huu' : u ≠ u') {p} (hp : p ∈ 
 lemma ball_subset (hu : u ∈ t) (hp : p ∈ t u) : ball (𝔠 p) (8 * D ^ 𝔰 p) ⊆ 𝓘 u :=
   t.ball_subset' hu hp
 
+<<<<<<< HEAD
 -- Used in the proof of Lemma 7.1.4
 variable {t} in
 lemma ball_subset_of_mem_𝓘 (hu : u ∈ t) {p : 𝔓 X} (hp : p ∈ t u) {x : X} (hx : x ∈ 𝓘 p) :
@@ -69,11 +70,21 @@ lemma ball_subset_of_mem_𝓘 (hu : u ∈ t) {p : 𝔓 X} (hp : p ∈ t u) {x : 
   refine (ball_subset_ball' ?_).trans (t.ball_subset hu hp)
   linarith [show dist x (𝔠 p) < 4 * D ^ (𝔰 p) from Grid_subset_ball hx]
 
+=======
+>>>>>>> 63d6334 (Lemma 7.5.1 (#194))
 lemma if_descendant_then_subset (hu : u ∈ t) (hp : p ∈ t u) : (𝓘 p : Set X) ⊆ 𝓘 u := by
   calc ↑(𝓘 p)
     _ ⊆ ball (𝔠 p) (4 * ↑D ^ 𝔰 p) := by
       exact GridStructure.Grid_subset_ball (i := 𝓘 p)
+<<<<<<< HEAD
     _ ⊆ ↑(𝓘 u) := ball_subset_of_mem_𝓘 hu hp Grid.c_mem_Grid
+=======
+    _ ⊆ ball (𝔠 p) (8 * ↑D ^ 𝔰 p) := by
+      gcongr
+      norm_num
+    _ ⊆ ↑(𝓘 u) := by
+      exact ball_subset t hu hp
+>>>>>>> 63d6334 (Lemma 7.5.1 (#194))
 
 end Forest
 
