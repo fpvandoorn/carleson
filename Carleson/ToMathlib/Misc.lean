cfc970d1b1b415e19bd5b839be01118e61439213
js2357 <James.Sundstrom@gmail.com>
2025-01-07 17:36:57 -0500

    Add comment "currently unused"

--- Carleson/ToMathlib/Misc.lean
+++ Carleson/ToMathlib/Misc.lean
@@ -388,6 +388,7 @@ theorem indicator_const {c : ℝ} {s: Set X}
 
 end Integrable
 
+-- Currently unused
 open Classical in
 theorem setIntegral_biUnion_le_sum_setIntegral {X : Type*} {ι : Type*} [MeasurableSpace X]
     {f : X → ℝ} (s : Finset ι) {S : ι → Set X} {μ : Measure X}
