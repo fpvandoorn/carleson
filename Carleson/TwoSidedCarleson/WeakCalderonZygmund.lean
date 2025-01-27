a116432347c8acc8334caf4c2666068d93c7b79a
Floris van Doorn <fpvdoorn@gmail.com>
2025-01-27 16:51:49 +0100

    reformulate some lemmas

--- Carleson/TwoSidedCarleson/WeakCalderonZygmund.lean
+++ Carleson/TwoSidedCarleson/WeakCalderonZygmund.lean
@@ -17,22 +17,23 @@ variable [CompatibleFunctions ℝ X (defaultA a)] [IsCancellative X (defaultτ a
 /-- The constant used in `nontangential_from_simple`. -/
 irreducible_def C10_2_1 (a : ℕ) : ℝ≥0 := 2 ^ (2 * a)
 
-/-- Lemma 10.2.1. -/
-theorem maximal_theorem (ha : 4 ≤ a)
-    {f : X → ℂ} (hmf : Measurable f) (hf : IsBounded (range f)) (h2f : volume (support f) < ∞)
-    {α : ℝ≥0} (hα : 0 < α):
-    α * distribution (globalMaximalFunction volume 1 f) α volume ≤ C10_2_1 a * eLpNorm f 1 := by
+/-- Lemma 10.2.1, formulated differently.
+The blueprint version is basically this after unfolding `HasBoundedWeakType`, `wnorm` and wnorm'`.
+-/
+theorem maximal_theorem (ha : 4 ≤ a) :
+    HasBoundedWeakType (globalMaximalFunction volume 1 : (X → ℂ) → X → ℝ≥0∞) 1 1 volume volume
+      (C10_2_1 a) := by
   sorry
 
 /-- The constant used in `czoperator_weak_1_1`. -/
 irreducible_def C10_0_3 (a : ℕ) : ℝ≥0 := 2 ^ (a ^ 3 + 19 * a)
 
-/-- Lemma 10.0.3 -/
+/-- Lemma 10.0.3, formulated differently.
+The blueprint version is basically this after unfolding `HasBoundedWeakType`, `wnorm` and wnorm'`.
+-/
 theorem czoperator_weak_1_1 (ha : 4 ≤ a)
-    (hT : ∃ r > 0, HasBoundedStrongType (CZOperator K r) 2 2 volume volume (C_Ts a))
-    {f : X → ℂ} (hmf : Measurable f) (hf : IsBounded (range f)) (h2f : volume (support f) < ∞)
-    {α : ℝ≥0} (hα : 0 < α) :
-    distribution (CZOperator K r f) α volume ≤ C10_0_3 a / α * ∫⁻ y, ‖f y‖ₑ := by
+    (hT : ∃ r > 0, HasBoundedStrongType (CZOperator K r) 2 2 volume volume (C_Ts a)) :
+    HasBoundedWeakType (CZOperator K r) 1 1 volume volume (C10_0_3 a) := by
   sorry
 
 
