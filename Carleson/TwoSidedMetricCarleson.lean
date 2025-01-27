657de33311a9b9802a9602353e644a3f3c1c86aa
Floris van Doorn <fpvdoorn@gmail.com>
2025-01-27 14:56:48 +0100

    section 10.2 statements

--- Carleson/TwoSidedMetricCarleson.lean
+++ Carleson/TwoSidedMetricCarleson.lean
@@ -147,6 +147,16 @@ theorem nontangential_from_simple (ha : 4 ≤ a)
 
 /-! ## Section 10.2 and Lemma 10.0.3 -/
 
+/-- The constant used in `nontangential_from_simple`. -/
+irreducible_def C10_2_1 (a : ℕ) : ℝ≥0 := 2 ^ (2 * a)
+
+/-- Lemma 10.2.1. -/
+theorem maximal_theorem (ha : 4 ≤ a)
+    {f : X → ℂ} (hmf : Measurable f) (hf : IsBounded (range f)) (h2f : volume (support f) < ∞)
+    {α : ℝ≥0} (hα : 0 < α):
+    α * distribution (globalMaximalFunction volume 1 f) α volume ≤ C10_2_1 a * eLpNorm f 1 := by
+  sorry
+
 /-- The constant used in `czoperator_weak_1_1`. -/
 irreducible_def C10_0_3 (a : ℕ) : ℝ≥0 := 2 ^ (a ^ 3 + 19 * a)
 
