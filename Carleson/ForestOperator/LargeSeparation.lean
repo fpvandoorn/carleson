463227b9584da911a76ce1065df9b2b72ecc6f50
Evgenia Karunus <lakesare@gmail.com>
2025-01-28 20:47:09 +0500

    refactor - use `calc`

--- Carleson/ForestOperator/LargeSeparation.lean
+++ Carleson/ForestOperator/LargeSeparation.lean
@@ -286,24 +286,23 @@ lemma first_estimate (hu₁ : u₁ ∈ t) (hu₂ : u₂ ∈ t) (hu : u₁ ≠ u�
     ← Real.logb_rpow (b:=D) (x:=s J) (by positivity) (by linarith)
   ]
   apply (Real.logb_le_logb DIsOne (by positivity) (by positivity)).mpr
-
-  have onOneHand : dist (c J) (𝔠 p) ≥ (D ^ s J / 4) := by
-    apply Forest.disjoint (p := 𝔠 p) (belongs := Grid.c_mem_Grid)
-    have inter_1 : (J : Set X) ∩ ball (c J) (D ^ s J / 4) = ball (c J) (D ^ s J / 4) := inter_eq_self_of_subset_right ball_subset_Grid
-    have inter_2 : (𝓘 u₁ : Set X) ∩ ↑J = ↑J := inter_eq_self_of_subset_right hJ.2.1
-    rw [← inter_1, ← inter_2]
-    apply Disjoint.inter_left
-    apply Disjoint.inter_left
-    rw [disjoint_comm]
-    by_contra notDisjoint
-    apply hp.2
-    apply overlap_implies_distance hu₁ hu₂ hu h2u (hpu₁ := notDisjoint)
-    right
-    exact hp.1
-  have onOtherHand : dist (c J) (𝔠 p) ≤ D ^ (s J) / 8 + 8 * D^(𝔰 p) := by
+  have bigger : D ^ (s J) / 8 + 8 * D^(𝔰 p) ≥ D ^ s J / (4 : ℝ) := by
     rw [not_disjoint_iff] at h
     rcases h with ⟨middleX, h1, h2⟩
-    calc dist (c J) (𝔠 p)
+    calc (D ^ s J / (4 : ℝ))
+    _ ≤ dist (c J) (𝔠 p) := by
+      apply Forest.disjoint (p := 𝔠 p) (belongs := Grid.c_mem_Grid)
+      have inter_1 : (J : Set X) ∩ ball (c J) (D ^ s J / 4) = ball (c J) (D ^ s J / 4) := inter_eq_self_of_subset_right ball_subset_Grid
+      have inter_2 : (𝓘 u₁ : Set X) ∩ ↑J = ↑J := inter_eq_self_of_subset_right hJ.2.1
+      rw [← inter_1, ← inter_2]
+      apply Disjoint.inter_left
+      apply Disjoint.inter_left
+      rw [disjoint_comm]
+      by_contra notDisjoint
+      apply hp.2
+      apply overlap_implies_distance hu₁ hu₂ hu h2u (hpu₁ := notDisjoint)
+      right
+      exact hp.1
     _ ≤ dist (𝔠 p) middleX + dist middleX (c J) := by
       rw [dist_comm]
       exact dist_triangle ..
@@ -318,14 +317,10 @@ lemma first_estimate (hu₁ : u₁ ∈ t) (hu₂ : u₂ ∈ t) (hu : u₁ ≠ u�
       rw [mem_ball, ← div_eq_inv_mul] at h2
       apply le_of_lt
       exact h2
-  rw [← ge_iff_le] at onOtherHand
-  have well := Trans.trans onOtherHand onOneHand
-  have white := sub_nonneg_of_le well
   apply le_neg_add_iff_le.mp
-  have red := mul_le_mul_of_nonneg_left (a := 8) white (by positivity)
-  ring_nf at red
-  exact_mod_cast red
-
+  have := mul_le_mul_of_nonneg_left (a := 8) (sub_nonneg_of_le bigger) (by positivity)
+  ring_nf at this
+  norm_cast
 
 theorem dist_triangle5 (a b c d e : X) :
   dist a e ≤ dist a b + dist b c + dist c d + dist d e :=
