5195551918a2c9926a5cff00ff380200856ce325
adomani <adomani@gmail.com>
2025-02-13 19:09:19 +0100

    include refinelinter

--- Carleson/Test.lean
+++ Carleson/Test.lean
@@ -1,6 +1,7 @@
 import Mathlib.Tactic.Linter.Header
 import Mathlib.Tactic.Linter.Multigoal
 import Mathlib.Tactic.Linter.OldObtain
+import Mathlib.Tactic.Linter.RefineLinter
 import Mathlib.Tactic.Linter.Style
 
 /-- error: unknown option 'linter.flexible' -/
