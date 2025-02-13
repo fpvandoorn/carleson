eb4c013d9789f18162f676e3115e3aa097ec1aa3
adomani <adomani@gmail.com>
2025-02-13 18:51:46 +0100

    reset more

--- Carleson/ToMathlib/Order/Chain.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.Order.Chain
-import Carleson.ToMathlib.Data.Set.Lattice
-
-lemma IsChain.pairwiseDisjoint_iUnion₂ {α β : Type*} [PartialOrder β] [OrderBot β]
-    (c : Set (Set α)) (hc : IsChain (· ⊆ ·) c) (f : α → β)
-    (h : ∀ s ∈ c, s.PairwiseDisjoint f) : (⋃ s ∈ c, s).PairwiseDisjoint f :=
-  hc.directedOn.pairwise_iUnion₂ _ _ h
