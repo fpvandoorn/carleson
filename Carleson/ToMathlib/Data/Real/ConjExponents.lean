79ec8d03d050b8fea166d780935668739178b0f2
js2357 <James.Sundstrom@gmail.com>
2025-02-15 12:31:48 -0500

    Modify proof of IsConjExponent.toReal

--- Carleson/ToMathlib/Data/Real/ConjExponents.lean
+++ Carleson/ToMathlib/Data/Real/ConjExponents.lean
@@ -10,11 +10,13 @@ variable {p q : ℝ≥0∞} (h : p.IsConjExponent q)
 section
 include h
 
-lemma toReal (hp : p ≠ ∞) (hq : q ≠ ∞) : p.toReal.IsConjExponent q.toReal := by
-  constructor
-  · rw [← ENNReal.ofReal_lt_iff_lt_toReal one_pos.le hp, ofReal_one]
+protected lemma toReal (hp : p ≠ ∞) (hq : q ≠ ∞) :
+    p.toReal.IsConjExponent q.toReal where
+  one_lt := by
+    rw [← ENNReal.ofReal_lt_iff_lt_toReal one_pos.le hp, ofReal_one]
     exact h.one_le.lt_of_ne (fun p_eq_1 ↦ hq (by simpa [p_eq_1] using h.conj_eq))
-  · rw [← toReal_inv, ← toReal_inv, ← toReal_add, h.inv_add_inv_conj, ENNReal.toReal_eq_one_iff]
+  inv_add_inv_conj := by
+    rw [← toReal_inv, ← toReal_inv, ← toReal_add, h.inv_add_inv_conj, ENNReal.toReal_eq_one_iff]
     · exact ENNReal.inv_ne_top.mpr h.ne_zero
     · exact ENNReal.inv_ne_top.mpr h.symm.ne_zero
 
