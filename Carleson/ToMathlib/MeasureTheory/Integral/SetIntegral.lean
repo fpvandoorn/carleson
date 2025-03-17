992f9e3ee3789d3501d25fc13ee677d4a3536fa4
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-03-17 16:45:08 +0100

    Extract auxiliary lemma

--- Carleson/ToMathlib/MeasureTheory/Integral/SetIntegral.lean
+++ Carleson/ToMathlib/MeasureTheory/Integral/SetIntegral.lean
@@ -4,17 +4,20 @@ open MeasureTheory
 
 /- Put after `setIntegral_re_add_im`, but probably refactor first to use `enorm` -/
 
+-- TODO: give a nice name!
+lemma aux {α 𝕜 : Type*} [RCLike 𝕜] {f : α → 𝕜} (x : α) :
+    starRingEnd 𝕜 (f x / ‖f x‖) * f x = ‖f x‖ := by
+  simp only [div_eq_inv_mul, map_mul, map_inv₀, RCLike.conj_ofReal, mul_assoc, RCLike.conj_mul]
+  norm_cast
+  rcases eq_or_ne (‖f x‖) 0 with hx | hx
+  · simp [hx]
+  · rw [pow_two, inv_mul_cancel_left₀ hx]
+
 lemma ennnorm_integral_starRingEnd_mul_eq_lintegral_ennnorm
     {𝕜 : Type*} [RCLike 𝕜] {α : Type*} [MeasurableSpace α] {μ : Measure α} {f : α → 𝕜}
     (hf : Integrable f μ) :
     ‖∫ x, starRingEnd 𝕜 (f x / ‖f x‖) * f x ∂μ‖₊ = ∫⁻ x, ‖f x‖₊ ∂μ := by
-  have A x : starRingEnd 𝕜 (f x / ‖f x‖) * f x = ‖f x‖ := by
-    simp only [div_eq_inv_mul, map_mul, map_inv₀, RCLike.conj_ofReal, mul_assoc, RCLike.conj_mul]
-    norm_cast
-    rcases eq_or_ne (‖f x‖) 0 with hx | hx
-    · simp [hx]
-    · rw [pow_two, inv_mul_cancel_left₀ hx]
-  simp_rw [A, integral_ofReal, nnnorm_algebraMap']
+  simp_rw [aux, integral_ofReal, nnnorm_algebraMap']
   rw [lintegral_coe_eq_integral]; swap
   · simpa only [coe_nnnorm] using hf.norm
   simp only [coe_nnnorm, ENNReal.ofReal, ENNReal.coe_inj]
@@ -34,13 +37,7 @@ lemma enorm_integral_starRingEnd_mul_eq_lintegral_enorm
     {𝕜 : Type*} [RCLike 𝕜] {α : Type*} [MeasurableSpace α] {μ : Measure α} {f : α → 𝕜}
     (hf : Integrable f μ) :
     ‖∫ x, starRingEnd 𝕜 (f x / ‖f x‖) * f x ∂μ‖ₑ = ∫⁻ x, ‖f x‖ₑ ∂μ := by
-  have A x : starRingEnd 𝕜 (f x / ‖f x‖) * f x = ‖f x‖ := by
-    simp only [div_eq_inv_mul, map_mul, map_inv₀, RCLike.conj_ofReal, mul_assoc, RCLike.conj_mul]
-    norm_cast
-    rcases eq_or_ne (‖f x‖) 0 with hx | hx
-    · simp [hx]
-    · rw [pow_two, inv_mul_cancel_left₀ hx]
-  simp_rw [A, integral_ofReal, enorm_algebraMap']
+  simp_rw [aux, integral_ofReal, enorm_algebraMap']
   simp_rw [enorm_eq_nnnorm]; rw [lintegral_coe_eq_integral]; swap
   · simpa only [coe_nnnorm] using hf.norm
   simp only [coe_nnnorm, ENNReal.ofReal, ENNReal.coe_inj]
