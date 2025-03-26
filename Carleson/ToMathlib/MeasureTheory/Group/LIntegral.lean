4dd582dbe51e71821d101dbb19a94cf8e2162608
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-03-26 16:54:15 +0100

    Update LIntegral.lean

--- Carleson/ToMathlib/MeasureTheory/Group/LIntegral.lean
+++ Carleson/ToMathlib/MeasureTheory/Group/LIntegral.lean
@@ -11,7 +11,7 @@ section MeasurableMul
 
 variable [Group G] [MeasurableMul G]
 
-/- MATHLIB PR: https://github.com/leanprover-community/mathlib4/pull/23341 -/
+/- Mathlib PR: https://github.com/leanprover-community/mathlib4/pull/23341 -/
 @[to_additive]
 theorem lintegral_div_left_eq_self [IsMulLeftInvariant μ] [MeasurableInv G] [IsInvInvariant μ]
     (f : G → ℝ≥0∞) (g : G) : (∫⁻ x, f (g / x) ∂μ) = ∫⁻ x, f x ∂μ := by
