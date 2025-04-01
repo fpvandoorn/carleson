98580db6a80484596e4f82ad3f0a7062f498d15d
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-04-01 12:49:15 +0200

    Apply suggestions from code review
    Co-authored-by: Floris van Doorn <fpvdoorn@gmail.com>

--- Carleson/HolderVanDerCorput.lean
+++ Carleson/HolderVanDerCorput.lean
@@ -439,5 +439,5 @@ def C2_0_5 (a : ℝ) : ℝ≥0 := 2 ^ (8 * a)
 theorem holder_van_der_corput {z : X} {R : ℝ≥0} (hR : 0 < R) {ϕ : X → ℂ}
     (hϕ : support ϕ ⊆ ball z R) (h2ϕ : iHolENorm ϕ z R < ∞) {f g : Θ X} :
     ‖∫ x, exp (I * (f x - g x)) * ϕ x‖₊ ≤
-    (C2_0_5 a : ℝ≥0∞) * volume (ball z R) * (iHolENorm ϕ z R).toNNReal *
+    (C2_0_5 a : ℝ≥0∞) * volume (ball z R) * iHolENorm ϕ z R *
     (1 + nndist_{z, R} f g) ^ (2 * a^2 + a^3 : ℝ)⁻¹ := sorry
--- Carleson/ToMathlib/Topology/Algebra/Support.lean
+++ Carleson/ToMathlib/Topology/Algebra/Support.lean
@@ -8,11 +8,5 @@ variable {α β : Type*} [TopologicalSpace α] [One β]
 @[to_additive]
 lemma ContinuousOn.continuous_of_mulTSupport_subset [TopologicalSpace β] {f : α → β}
     {s : Set α} (hs : ContinuousOn f s) (h's : IsOpen s) (h''s : mulTSupport f ⊆ s) :
-    Continuous f := by
-  apply continuous_iff_continuousAt.2 (fun x ↦ ?_)
-  by_cases hx : x ∈ s
-  · exact (hs x hx).continuousAt (h's.mem_nhds hx)
-  · have h'x : x ∈ (mulTSupport f)ᶜ := fun h ↦ hx (h''s h)
-    have : ContinuousOn f (mulTSupport f)ᶜ :=
-      (continuousOn_const (c := 1)).congr (fun x hx ↦ image_eq_one_of_nmem_mulTSupport hx)
-    apply (this x h'x).continuousAt (IsOpen.mem_nhds (by simp [isClosed_mulTSupport f]) h'x)
+    Continuous f :=
+  continuous_of_mulTSupport fun _ hx ↦ h's.continuousOn_iff.mp hs <| h''s hx
