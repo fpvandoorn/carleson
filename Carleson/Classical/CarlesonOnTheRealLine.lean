73308bad99234b8b4807717d1cf664f9b7d402fa
Leo Diedering <leo.diedering@web.de>
2025-05-02 15:17:24 +0200

    Added version of Lemma 11.1.5 for general exponents

--- Carleson/Classical/CarlesonOnTheRealLine.lean
+++ Carleson/Classical/CarlesonOnTheRealLine.lean
@@ -517,8 +517,20 @@ lemma carlesonOperatorReal_le_carlesonOperator : T ≤ carlesonOperator K := by
   rw [coeΘ_R_C]
   ring_nf
 
+/- Version of Lemma 11.1.5 for general exponents -/
+lemma rcarleson_general {q q' : ℝ≥0} (hq : q ∈ Set.Ioc 1 2) (hqq' : q.HolderConjugate q')
+    {F G : Set ℝ} (hF : MeasurableSet F) (hG : MeasurableSet G)
+    (f : ℝ → ℂ) (hmf : Measurable f) (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x) :
+    ∫⁻ x in G, T f x ≤ C10_0_1 4 q * (volume G) ^ (q' : ℝ)⁻¹ * (volume F) ^ (q : ℝ)⁻¹ := by
+  calc ∫⁻ x in G, T f x
+    _ ≤ ∫⁻ x in G, carlesonOperator K f x :=
+      lintegral_mono (carlesonOperatorReal_le_carlesonOperator _)
+    _ ≤ C10_0_1 4 q * (volume G) ^ (q' : ℝ)⁻¹ * (volume F) ^ (q : ℝ)⁻¹ :=
+      two_sided_metric_carleson (a := 4) (by norm_num) hq hqq' hF hG
+        Hilbert_strong_2_2 hmf hf
 
-/- Lemma 11.1.4 -/
+
+/- Lemma 11.1.5 -/
 lemma rcarleson {F G : Set ℝ} (hF : MeasurableSet F) (hG : MeasurableSet G)
     (f : ℝ → ℂ) (hmf : Measurable f) (hf : ∀ x, ‖f x‖ ≤ F.indicator 1 x) :
     ∫⁻ x in G, T f x ≤ C10_0_1 4 2 * (volume G) ^ (2 : ℝ)⁻¹ * (volume F) ^ (2 : ℝ)⁻¹ := by
@@ -526,11 +538,6 @@ lemma rcarleson {F G : Set ℝ} (hF : MeasurableSet F) (hG : MeasurableSet G)
     rw [NNReal.holderConjugate_iff_eq_conjExponent]
     · ext; norm_num
     norm_num
-  calc ∫⁻ x in G, T f x
-    _ ≤ ∫⁻ x in G, carlesonOperator K f x :=
-      lintegral_mono (carlesonOperatorReal_le_carlesonOperator _)
-    _ ≤ C10_0_1 4 2 * (volume G) ^ (2 : ℝ)⁻¹ * (volume F) ^ (2 : ℝ)⁻¹ :=
-      two_sided_metric_carleson (a := 4) (by norm_num) (by simp) conj_exponents hF hG
-        Hilbert_strong_2_2 hmf hf
+  exact rcarleson_general (by simp) conj_exponents hF hG f hmf hf
 
 end section
