cf0a693d21afc9edd6db46670192ea08a9976870
JasperMS <jasper.mulder@planet.nl>
2025-05-19 08:58:17 +0200

    [chore] Remaining PR comments 10.1.4 (#335)
    From PR #325.

--- Carleson/ToMathlib/HardyLittlewood.lean
+++ Carleson/ToMathlib/HardyLittlewood.lean
@@ -710,20 +710,6 @@ def globalMaximalFunction [μ.IsDoubling A] (p : ℝ) (u : X → E) (x : X) : �
   A ^ 2 * maximalFunction μ ((covering_separable_space X).choose ×ˢ (univ : Set ℤ))
     (·.1) (fun x ↦ 2 ^ (x.2)) p u x
 
--- prove only if needed. Use `MB_le_eLpNormEssSup`
-theorem globalMaximalFunction_lt_top {p : ℝ≥0} (hp₁ : 1 ≤ p)
-    {u : X → E} (hu : MemLp u ⊤ μ) {x : X} :
-    globalMaximalFunction μ p u x < ∞ := by
-  unfold globalMaximalFunction
-  rw [maximalFunction_eq_MB (by simp)]
-  apply mul_lt_top (by simp) (rpow_lt_top_of_nonneg (by simp) (lt_top_iff_ne_top.mp _))
-  have : MemLp (fun x ↦ ‖u x‖ ^ p.toReal) ⊤ μ := by
-    have rw1 : p.toReal = (p : ℝ≥0∞).toReal := by simp
-    have rw2 : (⊤ : ℝ≥0∞) = ⊤ / p := by simp
-    rw [rw1, rw2, memLp_norm_rpow_iff hu.aestronglyMeasurable (by positivity) (by simp)]
-    exact hu
-  exact lt_of_le_of_lt MB_le_eLpNormEssSup (this.eLpNormEssSup_lt_top)
-
 protected theorem MeasureTheory.AEStronglyMeasurable.globalMaximalFunction
     [BorelSpace X] {p : ℝ} {u : X → E} : AEStronglyMeasurable (globalMaximalFunction μ p u) μ :=
   AEStronglyMeasurable.maximalFunction countable_globalMaximalFunction
@@ -808,5 +794,18 @@ theorem globalMaximalFunction_ae_lt_top [BorelSpace X] [IsFiniteMeasureOnCompact
   conv => arg 1; intro x; rw [← enorm_eq_self (x := globalMaximalFunction μ p₁ u x)]
   exact MemWLp.ae_ne_top (HasWeakType.memWLp (hasWeakType_globalMaximalFunction hp₁ hp₁₂.le) hu
     C_weakType_globalMaximalFunction_lt_top)
+
+theorem globalMaximalFunction_lt_top {p : ℝ≥0} (hp₁ : 1 ≤ p)
+    {u : X → E} (hu : MemLp u ⊤ μ) {x : X} :
+    globalMaximalFunction μ p u x < ∞ := by
+  unfold globalMaximalFunction
+  rw [maximalFunction_eq_MB (by simp)]
+  apply mul_lt_top (by simp) (rpow_lt_top_of_nonneg (by simp) (lt_top_iff_ne_top.mp _))
+  have : MemLp (fun x ↦ ‖u x‖ ^ p.toReal) ⊤ μ := by
+    have rw1 : p.toReal = (p : ℝ≥0∞).toReal := by simp
+    have rw2 : (⊤ : ℝ≥0∞) = ⊤ / p := by simp
+    rw [rw1, rw2, memLp_norm_rpow_iff hu.aestronglyMeasurable (by positivity) (by simp)]
+    exact hu
+  exact lt_of_le_of_lt MB_le_eLpNormEssSup (this.eLpNormEssSup_lt_top)
 
 end GMF
