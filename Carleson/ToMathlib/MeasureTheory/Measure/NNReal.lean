aa6250b1330702e79b165cb49aea892c3aba405f
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-05-20 14:11:59 +0200

    State some future lemmas

--- Carleson/ToMathlib/MeasureTheory/Measure/NNReal.lean
+++ Carleson/ToMathlib/MeasureTheory/Measure/NNReal.lean
@@ -1,6 +1,6 @@
 import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
 
-open MeasureTheory
+open MeasureTheory Set
 open scoped NNReal ENNReal
 
 noncomputable section
@@ -13,8 +13,8 @@ lemma NNReal.volume_apply_measurableSet {s : Set ℝ≥0} (hs : MeasurableSet s)
   MeasureTheory.Measure.map_apply_of_aemeasurable (by fun_prop) hs
 
 -- sanity check: this measure is what you expect
-example : volume (Set.Ioo (3 : ℝ≥0) 5) = 2 := by
-  have : Real.toNNReal ⁻¹' Set.Ioo 3 5 = Set.Ioo (3 : ℝ) 5 := by ext; simp
+example : volume (Ioo (3 : ℝ≥0) 5) = 2 := by
+  have : Real.toNNReal ⁻¹' Ioo 3 5 = Ioo (3 : ℝ) 5 := by ext; simp
   rw [NNReal.volume_apply_measurableSet measurableSet_Ioo, this]
   simpa only [Real.volume_Ioo, ENNReal.ofReal_eq_ofNat] using by norm_num
 
@@ -36,5 +36,14 @@ example : volume (Set.Icc (3 : ℝ≥0∞) 42) = 39 := by
   have : ENNReal.ofReal ⁻¹' Set.Icc 3 42 = Set.Icc (3 : ℝ) 42 := by ext; simp
   rw [ENNReal.volume_apply_measurableSet measurableSet_Icc, this]
   simpa using by norm_num
+
+-- Future: prove these integral lemmas and name them properly
+lemma todo (f : ℝ≥0∞ → ℝ≥0∞) : ∫⁻ x : ℝ≥0, f x = ∫⁻ x in Ioi (0 : ℝ), f (.ofReal x) := sorry
+
+lemma todo' (f : ℝ≥0 → ℝ≥0∞) : ∫⁻ x : ℝ≥0, f x = ∫⁻ x in Ioi (0 : ℝ), f (Real.toNNReal x) := sorry
+
+lemma todo'' (f : ℝ → ℝ≥0∞) : ∫⁻ x : ℝ≥0, f (x.toReal) = ∫⁻ x in Ioi (0 : ℝ), f x := sorry
+
+-- Future: lemmas about interaction with the Bochner integral
 
 end
