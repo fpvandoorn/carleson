8458ded0d03dca290159850d520daeb5a5c897d2
js2357 <James.Sundstrom@gmail.com>
2025-07-12 03:42:17 -0400

    change variable

--- Carleson/ToMathlib/WeakType.lean
+++ Carleson/ToMathlib/WeakType.lean
@@ -524,11 +524,11 @@ lemma HasBoundedStrongType.hasBoundedWeakType (hp' : 0 < p')
     ⟨(h f hf).1, wnorm_le_eLpNorm (h f hf).1 hp' |>.trans (h f hf).2⟩
 
 lemma HasBoundedStrongType.const_smul {T : (α → ε₁) → α' → ℝ≥0∞}
-    (h : HasBoundedStrongType T p p' μ ν c) (x : ℝ≥0) :
-    HasBoundedStrongType (x • T) p p' μ ν (x • c) := by
+    (h : HasBoundedStrongType T p p' μ ν c) (r : ℝ≥0) :
+    HasBoundedStrongType (r • T) p p' μ ν (r • c) := by
   intro f hf
   rw [Pi.smul_apply, MeasureTheory.eLpNorm_const_smul']
-  exact ⟨(h f hf).1.const_smul _, le_of_le_of_eq (mul_le_mul_left' (h f hf).2 ‖x‖ₑ) (by simp; rfl)⟩
+  exact ⟨(h f hf).1.const_smul _, le_of_le_of_eq (mul_le_mul_left' (h f hf).2 ‖r‖ₑ) (by simp; rfl)⟩
 
 end HasBoundedStrongType
 
