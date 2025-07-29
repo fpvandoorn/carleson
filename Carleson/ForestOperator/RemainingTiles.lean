71db259b00fd7f896c6c4d4fcba93bcffc2274c9
Sebastien Gouezel <sebastien.gouezel@univ-rennes1.fr>
2025-07-29 17:39:01 +0200

    chore: adjust the constants in the blueprint to the formalization (#473)
    Also optimize a tiny bit one argument, giving very slightly better
    constants in the end.
    
    And fix the hyperlinks in the pdf file of the blueprint.

--- Carleson/ForestOperator/RemainingTiles.lean
+++ Carleson/ForestOperator/RemainingTiles.lean
@@ -807,7 +807,8 @@ lemma cntp_approxOnCube_eq (hu₁ : u₁ ∈ t) :
       rw [Finset.mem_filter_univ, 𝓙₆, mem_inter_iff, mem_Iic] at mp
       rw [indicator_of_mem (mp.2.1 my)]
 
-/-- The constant used in `correlation_near_tree_parts`. -/
+/-- The constant used in `correlation_near_tree_parts`.
+Has value `2 ^ (232 * a ^ 3 + 21 * a + 5- 25/(101a) * Z n κ)` in the blueprint. -/
 irreducible_def C7_4_6 (a n : ℕ) : ℝ≥0 := C7_2_1 a * C7_6_2 a n
 
 /-- Lemma 7.4.6 -/
