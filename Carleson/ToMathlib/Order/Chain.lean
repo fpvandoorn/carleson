5bf8d9e4bcbf1e79eead8f3236c65f17495b4683
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-10-24 12:36:39 +0200

    wip: fix the build

--- Carleson/ToMathlib/Order/Chain.lean
+++ Carleson/ToMathlib/Order/Chain.lean
@@ -1,10 +1,2 @@
 import Mathlib.Order.Preorder.Chain
 import Mathlib.Data.Set.Lattice
-
--- Upstreaming status: has been upstreamed already; will be removed in the next mathlib bump
-
-lemma IsChain.pairwiseDisjoint_iUnion₂ {α β : Type*} [PartialOrder β] [OrderBot β]
-    (c : Set (Set α)) (hc : IsChain (· ⊆ ·) c) (f : α → β)
-    (h : ∀ s ∈ c, s.PairwiseDisjoint f) : (⋃ s ∈ c, s).PairwiseDisjoint f := by
-  apply Set.pairwise_iUnion₂ (directedOn hc)
-  exact fun s hs ↦ h s hs
