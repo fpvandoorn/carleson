f88f3c82bb19aed9c0de78236e194a73fc678182
Michael Rothgang <rothgang@math.uni-bonn.de>
2025-11-03 21:21:06 +0100

    One more

--- Carleson/ToMathlib/Data/Real/ConjExponents.lean
+++ Carleson/ToMathlib/Data/Real/ConjExponents.lean
@@ -5,6 +5,8 @@ open scoped ENNReal
 variable {p q : ℝ≥0∞} (h : p.HolderConjugate q)
 include h
 
+-- Upstreaming status: seems useful to have and good to go
+
 protected lemma ENNReal.HolderConjugate.toReal_of_ne_top (hp : p ≠ ∞) (hq : q ≠ ∞) :
     p.toReal.HolderConjugate q.toReal where
   left_pos := toReal_pos (HolderConjugate.ne_zero p q) hp
