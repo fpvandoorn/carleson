8a380c7b0dd85e66d761206f854053ff6f52b0b3
Leo Diedering <leo.diedering@web.de>
2025-12-15 13:45:38 +0100

    Use fun_prop for measurability in AEMeasurable.lean

--- Carleson/ToMathlib/MeasureTheory/Measure/AEMeasurable.lean
+++ Carleson/ToMathlib/MeasureTheory/Measure/AEMeasurable.lean
@@ -9,8 +9,7 @@ lemma aeMeasurable_withDensity_inv {f : NNReal → ENNReal} (hf : AEMeasurable f
     AEMeasurable f (volume.withDensity (fun t ↦ t⁻¹)) := by
   have : AEMeasurable f (volume.withDensity (fun t ↦ ENNReal.ofNNReal t⁻¹)) := by
     rw [aemeasurable_withDensity_ennreal_iff measurable_inv]
-    apply AEMeasurable.mul _ hf
-    exact measurable_inv.aemeasurable.coe_nnreal_ennreal
+    fun_prop
   convert this using 1
   rw [withDensity_eq_iff_of_sigmaFinite]
   · rw [Filter.eventuallyEq_iff_exists_mem]
@@ -22,8 +21,7 @@ lemma aeMeasurable_withDensity_inv {f : NNReal → ENNReal} (hf : AEMeasurable f
     · intro x hx
       simp only [ne_eq, Set.mem_setOf_eq] at *
       exact (ENNReal.coe_inv hx).symm
-  · apply Measurable.aemeasurable
-    measurability
-  · exact measurable_inv.aemeasurable.coe_nnreal_ennreal
+  · fun_prop
+  · fun_prop
 
 end MeasureTheory
