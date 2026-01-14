3b2c6a3a6c31af87b1e599985152987c8e0df2f1
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-01-14 14:16:58 +0100

    Apply suggestions from code review

--- Carleson/ToMathlib/MeasureTheory/Measure/AEMeasurable.lean
+++ Carleson/ToMathlib/MeasureTheory/Measure/AEMeasurable.lean
@@ -1,10 +1,9 @@
 import Carleson.ToMathlib.MeasureTheory.Measure.NNReal
 import Mathlib.MeasureTheory.Function.AEEqOfLIntegral
 
-
 namespace MeasureTheory
 
---TODO: generalize this to other measures and density functions?
+-- TODO: generalize this to other measures and density functions?
 lemma aeMeasurable_withDensity_inv {f : NNReal → ENNReal} (hf : AEMeasurable f) :
     AEMeasurable f (volume.withDensity (fun t ↦ t⁻¹)) := by
   have : AEMeasurable f (volume.withDensity (fun t ↦ ENNReal.ofNNReal t⁻¹)) := by
