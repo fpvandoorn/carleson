3fefeb9301ca2b11dd520d249b21f745aadad5b0
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-04-30 17:43:54 +0200

    chore: remove lemma now upstreamed, and minor golf

--- Carleson/ToMathlib/MeasureTheory/Integral/IntegrableOn.lean
+++ Carleson/ToMathlib/MeasureTheory/Integral/IntegrableOn.lean
@@ -1,15 +1,9 @@
 import Mathlib.MeasureTheory.Integral.IntegrableOn
 
-open Set Filter TopologicalSpace MeasureTheory Function
-
-open scoped Topology Interval Filter ENNReal MeasureTheory
-
 -- Upstreaming note: Hypotheses and variables have been matched to corresponding Mathlib file
 -- `IntegrableAtFilter.congr` already in mathlib.
--- `integrableOn_of_integrableOn_inter_support` already in mathlib as
--- `MeasureTheory.IntegrableOn.of_inter_support`.
 
-variable {α β ε ε' E F : Type*} [MeasurableSpace α]
+variable {α β ε ε' : Type*} [MeasurableSpace α]
 
 namespace MeasureTheory
 
@@ -22,16 +16,3 @@ protected theorem IntegrableAtFilter.congr'_enorm {μ : Measure α} [Topological
   Exists.casesOn hf fun s hs ↦ ⟨s, hs.1, hs.2.congr'_enorm hg.restrict (ae_restrict_le h)⟩
 
 end MeasureTheory
-
-section NormedAddCommGroup
-
-variable [NormedAddCommGroup E] {f g : α → ε'} {s t : Set α} {μ ν : Measure α}
-variable [TopologicalSpace ε'] [ENormedAddMonoid ε']
-
-theorem integrableOn_of_integrableOn_inter_support [PseudoMetrizableSpace ε'] {f : α → ε'}
-  (hs : MeasurableSet s) (hf : IntegrableOn f (s ∩ support f) μ) :
-    IntegrableOn f s μ := by
-  apply IntegrableOn.of_forall_diff_eq_zero hf hs
-  simp
-
-end NormedAddCommGroup
