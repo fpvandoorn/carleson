54a5b6c14051ac79387b89a9dbc03c16a4496296
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-30 12:10:56 +0200

    Now the solution file works

--- Carleson/Solution.lean
+++ Carleson/Solution.lean
@@ -12,14 +12,10 @@ open scoped NNReal
 
 namespace Solution
 
--- Carleson's theorem asserting a.e. convergence of the partial Fourier sums for continous functions
 open Real in
-theorem ClassicalCarleson {f : ℝ → ℂ} (cont_f : Continuous f) (periodic_f : f.Periodic (2 * π)) :
+theorem classical_carleson' {f : ℝ → ℂ} (cont_f : Continuous f) (periodic_f : f.Periodic (2 * π)) :
     ∀ᵐ x, Filter.Tendsto (partialFourierSum · f x) Filter.atTop (nhds (f x)) :=
   classical_carleson cont_f periodic_f
-
-/-- The constant used in `MetricSpaceCarleson` and `LinearizedMetricCarleson` -/
-def C (a : ℕ) (q : ℝ≥0) : ℝ≥0 := 2 ^ (443 * a ^ 3) / (q - 1) ^ 6
 
 theorem metric_carleson'
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
@@ -27,10 +23,9 @@ theorem metric_carleson'
     (hqq' : q.HolderConjugate q') (mF : MeasurableSet F) (mG : MeasurableSet G)
     (mf : Measurable f) (nf : (‖f ·‖) ≤ F.indicator 1)
     (hT : HasBoundedStrongType (nontangentialOperator K · ·) 2 2 volume volume (2 ^ a ^ 3)) :
-    ∫⁻ x in G, carlesonOperator K f x ≤ (C a q) * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ := by
-  convert metric_carleson hq hqq' mF mG mf nf hT (a := a)
-  unfold C C1_0_2
-  sorry -- unfold 𝕔; norm_num
+    ∫⁻ x in G, carlesonOperator K f x ≤
+      (C1_0_2 a q) * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
+  metric_carleson hq hqq' mF mG mf nf hT (a := a)
 
 theorem linearized_metric_carleson'
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
@@ -40,8 +35,7 @@ theorem linearized_metric_carleson'
     (hT : ∀ θ : Θ X, HasBoundedStrongType (linearizedNontangentialOperator Q θ K · ·)
       2 2 volume volume (2 ^ a ^ 3)) :
     ∫⁻ x in G, linearizedCarlesonOperator Q K f x ≤
-      C a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ := by
-  convert linearized_metric_carleson hq hqq' mF mG mf nf hT
-  sorry -- same story as above
+      C1_0_2 a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
+  linearized_metric_carleson hq hqq' mF mG mf nf hT
 
 end Solution
