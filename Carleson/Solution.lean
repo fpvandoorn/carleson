5f73eb30a86e61d52f528896d06403cb3ab73ea4
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-30 11:35:58 +0200

    Solution file almost works: last question is the unfolding of the constants

--- Carleson/Solution.lean
+++ Carleson/Solution.lean
@@ -7,7 +7,7 @@ public import Carleson.BasicDefinitions
 
 public noncomputable section
 
-open MeasureTheory Set Challenge
+open MeasureTheory Set
 open scoped NNReal
 
 namespace Solution
@@ -15,31 +15,33 @@ namespace Solution
 -- Carleson's theorem asserting a.e. convergence of the partial Fourier sums for continous functions
 open Real in
 theorem ClassicalCarleson {f : ℝ → ℂ} (cont_f : Continuous f) (periodic_f : f.Periodic (2 * π)) :
-    ∀ᵐ x, Filter.Tendsto (Challenge.partialFourierSum · f x) Filter.atTop (nhds (f x)) :=
+    ∀ᵐ x, Filter.Tendsto (partialFourierSum · f x) Filter.atTop (nhds (f x)) :=
   classical_carleson cont_f periodic_f
 
 /-- The constant used in `MetricSpaceCarleson` and `LinearizedMetricCarleson` -/
 def C (a : ℕ) (q : ℝ≥0) : ℝ≥0 := 2 ^ (443 * a ^ 3) / (q - 1) ^ 6
 
-theorem metric_carleson
+theorem metric_carleson'
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
-    [Challenge.KernelProofData a K] {f : X → ℂ} [Challenge.IsCancellative X (Challenge.defaultτ a)] (hq : q ∈ Ioc 1 2)
+    [KernelProofData a K] {f : X → ℂ} [IsCancellative X (defaultτ a)] (hq : q ∈ Ioc 1 2)
     (hqq' : q.HolderConjugate q') (mF : MeasurableSet F) (mG : MeasurableSet G)
     (mf : Measurable f) (nf : (‖f ·‖) ≤ F.indicator 1)
-    (hT : Challenge.HasBoundedStrongType (nontangentialOperator K · ·) 2 2 volume volume (2 ^ a ^ 3)) :
-    ∫⁻ x in G, carlesonOperator K f x ≤ C a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ := by
-  sorry -- apply _root_.metric_carleson hq hqq' --mF mG mf nf hT (a := a)
+    (hT : HasBoundedStrongType (nontangentialOperator K · ·) 2 2 volume volume (2 ^ a ^ 3)) :
+    ∫⁻ x in G, carlesonOperator K f x ≤ (C a q) * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ := by
+  convert metric_carleson hq hqq' mF mG mf nf hT (a := a)
+  unfold C C1_0_2
+  sorry -- unfold 𝕔; norm_num
 
-theorem linearized_metric_carleson
+theorem linearized_metric_carleson'
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
-    [Challenge.KernelProofData a K] {Q : SimpleFunc X (Θ X)} {f : X → ℂ}
-    [Challenge.IsCancellative X (Challenge.defaultτ a)]
+    [KernelProofData a K] {Q : SimpleFunc X (Θ X)} {f : X → ℂ} [IsCancellative X (defaultτ a)]
     (hq : q ∈ Ioc 1 2) (hqq' : q.HolderConjugate q') (mF : MeasurableSet F) (mG : MeasurableSet G)
     (mf : Measurable f) (nf : (‖f ·‖) ≤ F.indicator 1)
-    (hT : ∀ θ : Θ X, Challenge.HasBoundedStrongType (linearizedNontangentialOperator Q θ K · ·)
+    (hT : ∀ θ : Θ X, HasBoundedStrongType (linearizedNontangentialOperator Q θ K · ·)
       2 2 volume volume (2 ^ a ^ 3)) :
     ∫⁻ x in G, linearizedCarlesonOperator Q K f x ≤
-      C a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
-  sorry
+      C a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ := by
+  convert linearized_metric_carleson hq hqq' mF mG mf nf hT
+  sorry -- same story as above
 
 end Solution
