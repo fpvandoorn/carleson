0a6602a15095cb89f65ef34e3a6d87945b8c2267
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-06-30 14:56:18 +0200

    Final comparator config

--- Solution.lean
+++ Solution.lean
@@ -1,29 +1,30 @@
 module
 
+public import Carleson.Classical.ClassicalCarleson
+public import Carleson.MetricCarleson.Main
 public import Carleson.BasicDefinitions
 
-/-! # Challenge file for using comparator -/
+/-! # Solution file for using comparator -/
 
 public noncomputable section
 
 open MeasureTheory Set
 open scoped NNReal
 
-namespace Challenge
-
--- Carleson's theorem asserting a.e. convergence of the partial Fourier sums for continous functions
 open Real in
 theorem classical_carleson' {f : ℝ → ℂ} (cont_f : Continuous f) (periodic_f : f.Periodic (2 * π)) :
-  ∀ᵐ x, Filter.Tendsto (partialFourierSum · f x) Filter.atTop (nhds (f x)) := sorry
+    ∀ᵐ x, Filter.Tendsto (partialFourierSum · f x) Filter.atTop (nhds (f x)) :=
+  classical_carleson cont_f periodic_f
 
-theorem metric_carleson'
+theorem metric_carleson''
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
     [KernelProofData a K] {f : X → ℂ} [IsCancellative X (defaultτ a)] (hq : q ∈ Ioc 1 2)
     (hqq' : q.HolderConjugate q') (mF : MeasurableSet F) (mG : MeasurableSet G)
     (mf : Measurable f) (nf : (‖f ·‖) ≤ F.indicator 1)
     (hT : HasBoundedStrongType (nontangentialOperator K · ·) 2 2 volume volume (2 ^ a ^ 3)) :
-    ∫⁻ x in G, carlesonOperator K f x ≤ C1_0_2 a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
-  sorry
+    ∫⁻ x in G, carlesonOperator K f x ≤
+      (C1_0_2 a q) * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
+  metric_carleson hq hqq' mF mG mf nf hT (a := a)
 
 theorem linearized_metric_carleson'
     {X : Type*} {a : ℕ} [MetricSpace X] {q q' : ℝ≥0} {F G : Set X} {K : X → X → ℂ}
@@ -34,6 +35,4 @@ theorem linearized_metric_carleson'
       2 2 volume volume (2 ^ a ^ 3)) :
     ∫⁻ x in G, linearizedCarlesonOperator Q K f x ≤
       C1_0_2 a q * volume G ^ (q' : ℝ)⁻¹ * volume F ^ (q : ℝ)⁻¹ :=
-  sorry
-
-end Challenge
+  linearized_metric_carleson hq hqq' mF mG mf nf hT
