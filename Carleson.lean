ad7cdc493f08ca84fc72434ecd1290b4d833f286
Michael Rothgang <rothgang@math.uni-bonn.de>
2026-07-14 00:26:30 +0200

    Update

--- Carleson.lean
+++ Carleson.lean
@@ -1,4 +1,4 @@
-module  -- shake: keep-all
+module  -- shake: keep-all --deprecated_module: ignore
 
 public import Carleson.Antichain.AntichainOperator
 public import Carleson.Antichain.AntichainTileCount
