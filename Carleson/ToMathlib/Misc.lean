48c428f5ab8276a2998efa6b9edfa845509f310c
Evgenia Karunus <lakesare@gmail.com>
2026-08-22 22:58:21 +0100

    Misc.lean - warnings

--- Carleson/ToMathlib/Misc.lean
+++ Carleson/ToMathlib/Misc.lean
@@ -395,24 +395,24 @@ protected noncomputable def out (x : α) : α :=
   if hx : x ∈ s then (Quotient.out (s := hr.setoid) ⟦⟨x, hx⟩⟧ : s) else x
 
 lemma out_mem (hx : x ∈ s) : hr.out x ∈ s := by
-  rw [EquivalenceOn.out, dif_pos hx]
+  rw [EquivalenceOn.out, dite_eq_left hx]
   apply Subtype.prop
 
 @[simp]
 lemma out_mem_iff : hr.out x ∈ s ↔ x ∈ s := by
   refine ⟨fun h ↦ ?_, out_mem⟩
   by_contra hx
-  rw [EquivalenceOn.out, dif_neg hx] at h
+  rw [EquivalenceOn.out, dite_eq_right hx] at h
   exact hx h
 
 lemma out_rel (hx : x ∈ s) : r (hr.out x) x := by
-  rw [EquivalenceOn.out, dif_pos hx]
+  rw [EquivalenceOn.out, dite_eq_left hx]
   exact @Quotient.mk_out _ (hr.setoid) ⟨x, hx⟩
 
 lemma rel_out (hx : x ∈ s) : r x (hr.out x) := hr.symm (out_mem hx) hx (out_rel hx)
 
 lemma out_inj (hx : x ∈ s) (hy : y ∈ s) (h : r x y) : hr.out x = hr.out y := by
-  simp_rw [EquivalenceOn.out, dif_pos hx, dif_pos hy]
+  simp_rw [EquivalenceOn.out, dite_eq_left hx, dite_eq_left hy]
   congr 1
   simp_rw [Quotient.out_inj, Quotient.eq]
   exact h
